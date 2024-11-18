b7a387d965d0d9de1449aa87a5fe7e4897f59e05
yousef <YoussefSaad299@gmail.com>
2024-11-18 16:12:39 +0200

    chore: add entities(models) in Talabat.Core

---
+++ Talabat.Core/Entities/BaseEntity.cs
@@ -0,0 +1,6 @@
+namespace Talabat.Core.Entities;
+
+public class BaseEntity
+{
+    public int Id { get; set; }
+}
---
+++ Talabat.Core/Entities/Product.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Talabat.Core.Entities;
+
+public class Product : BaseEntity
+{
+    public string Name { get; set; } = null!;
+    
+    public string Description { get; set; } = null!;
+    
+    public string PictureUrl { get; set; } = null!;
+    
+    public decimal Price { get; set; }
+    
+    // Product & Brand
+    public int BrandId { get; set; } // FK Column => ProductBrand 
+    
+    public ProductBrand Brand { get; set; } = null!; // Navigational property [ONE]
+    
+    // Product & Category
+    public int CategoryId { get; set; } // FK Column => ProductCategory
+    
+    public ProductCategory Category { get; set; } = null!; // Navigational property [ONE]
+}
